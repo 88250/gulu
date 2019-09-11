1b5ed3dc661b063bb4c09f20e3b8fb96480b3f80
王漩波 <1345545983@qq.com>
2019-09-11 17:44:22 +0800

    :sparkles: 新增获取本机第一张网卡的mac地址方法

--- net.go
+++ net.go
@@ -38,3 +38,29 @@ func (*GuluNet) LocalIP() (string, error) {
 
 	return "", errors.New("can't get local IP")
 }
+
+// LocalMac gets the first NIC's mac address.
+func (*GuluNet) LocalMac() (string, error) {
+	interfaces, err := net.Interfaces()
+	if err != nil {
+		return "", err
+	}
+
+	for _, inter := range interfaces {
+		address, err := inter.Addrs()
+		if err != nil {
+			return "", err
+		}
+
+		for _, address := range address {
+			// check the address type and if it is not a loopback the display it
+			if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+				if ipnet.IP.To4() != nil {
+					return inter.HardwareAddr.String(), nil
+				}
+			}
+		}
+	}
+
+	return "", errors.New("can't get local mac")
+}
