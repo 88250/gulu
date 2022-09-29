da1dc91c93435f5c2ff8f422904e5051701a77e4
Liang Ding <d@b3log.org>
2022-09-29 20:34:04 +0800

    :art: 增加 cmd 属性设置工具函数

---
+++ cmdattr.go
@@ -0,0 +1,18 @@
+// Gulu - Golang common utilities for everyone.
+// Copyright (c) 2019-present, b3log.org
+//
+// Gulu is licensed under Mulan PSL v2.
+// You can use this software according to the terms and conditions of the Mulan PSL v2.
+// You may obtain a copy of Mulan PSL v2 at:
+//         http://license.coscl.org.cn/MulanPSL2
+// THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+// See the Mulan PSL v2 for more details.
+
+//go:build !windows
+
+package gulu
+
+import "os/exec"
+
+func CmdAttr(cmd *exec.Cmd) {
+}
---
+++ cmdattr_windows.go
@@ -0,0 +1,20 @@
+// Gulu - Golang common utilities for everyone.
+// Copyright (c) 2019-present, b3log.org
+//
+// Gulu is licensed under Mulan PSL v2.
+// You can use this software according to the terms and conditions of the Mulan PSL v2.
+// You may obtain a copy of Mulan PSL v2 at:
+//         http://license.coscl.org.cn/MulanPSL2
+// THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+// See the Mulan PSL v2 for more details.
+
+package gulu
+
+import (
+	"os/exec"
+	"syscall"
+)
+
+func CmdAttr(cmd *exec.Cmd) {
+	cmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
+}
